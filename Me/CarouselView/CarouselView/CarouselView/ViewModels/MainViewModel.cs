ef1f1b6409337eed5f95b04d8191be02996b3efb
Edwin Ramos <E_noel-r.R96@hotmail.com>
2019-11-03 15:47:56 -0600

    Carousel View

---
+++ Me/CarouselView/CarouselView/CarouselView/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CarouselView.ViewModels
+{
+    public class MainViewModel
+    {
+        public Command NavigateCommand { get; set; }
+        public Type pageType { get; set; }
+        public MainViewModel()
+        {
+            NavigateCommand = new Command<Type>(NavigatePage);
+        }
+
+        private async void NavigatePage(Type obj)
+        {
+            Page page = (Page)Activator.CreateInstance(obj);
+            await App.Current.MainPage.Navigation.PushAsync(page);
+        }
+    }
+}
